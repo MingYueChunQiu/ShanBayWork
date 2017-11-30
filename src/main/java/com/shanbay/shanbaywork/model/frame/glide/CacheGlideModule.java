c64b693aef9eb49db9466a05db1653c9c7b97d46
MingYueChunQiu <xiyujieit@163.com>
2017-11-30 23:19:31 +0800

    编写可选择文本的自定义View控件。

--- src/main/java/com/shanbay/shanbaywork/model/frame/glide/CacheGlideModule.java
+++ src/main/java/com/shanbay/shanbaywork/model/frame/glide/CacheGlideModule.java
@@ -1,13 +1,22 @@
 package com.shanbay.shanbaywork.model.frame.glide;
 
 import android.content.Context;
+import android.os.Environment;
+import android.support.annotation.Nullable;
+import android.util.Log;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.GlideBuilder;
 import com.bumptech.glide.Registry;
+import com.bumptech.glide.load.engine.cache.DiskCache;
 import com.bumptech.glide.load.engine.cache.DiskLruCacheFactory;
+import com.bumptech.glide.load.engine.cache.DiskLruCacheWrapper;
+import com.bumptech.glide.load.engine.cache.ExternalPreferredCacheDiskCacheFactory;
+import com.bumptech.glide.load.engine.cache.InternalCacheDiskCacheFactory;
 import com.bumptech.glide.load.engine.cache.MemorySizeCalculator;
 import com.bumptech.glide.module.GlideModule;
+
+import java.io.File;
 
 /**
  * Created by 明月春秋 on 2017/11/29.
@@ -17,11 +26,19 @@ import com.bumptech.glide.module.GlideModule;
 public class CacheGlideModule implements GlideModule {
 
     @Override
-    public void applyOptions(Context context, GlideBuilder builder) {
+    public void applyOptions(final Context context, GlideBuilder builder) {
         MemorySizeCalculator calculator = new MemorySizeCalculator.Builder(context).build();
-        int size = calculator.getMemoryCacheSize();
+        final int size = calculator.getMemoryCacheSize();
         String fileDirectory = "images";
-        builder.setDiskCache(new DiskLruCacheFactory(fileDirectory, size));
+        builder.setDiskCache(new DiskCache.Factory() {
+            @Nullable
+            @Override
+            public DiskCache build() {
+                File file = new File(context.getExternalCacheDir(), "images");
+                file.mkdirs();
+                return DiskLruCacheWrapper.get(file, size);
+            }
+        });
     }
 
     @Override
--- src/main/java/com/shanbay/shanbaywork/view/SelectableView.java
+++ src/main/java/com/shanbay/shanbaywork/view/SelectableView.java
@@ -1,8 +1,10 @@
 package com.shanbay.shanbaywork.view;
 
 import android.content.Context;
+import android.graphics.Canvas;
 import android.support.annotation.Nullable;
 import android.text.Layout;
+import android.text.StaticLayout;
 import android.text.TextPaint;
 import android.util.AttributeSet;
 import android.widget.TextView;
@@ -12,6 +14,7 @@ import android.widget.TextView;
  * 自定义文本View
  * 继承自AppCompatTextView
  * 实现文本对齐，以及单词可被点击选中效果
+ * 本view部分内容参考devilist的CSDN博客
  */
 
 public class SelectableView extends android.support.v7.widget.AppCompatTextView {
@@ -24,7 +27,60 @@ public class SelectableView extends android.support.v7.widget.AppCompatTextView 
         super(context, attrs);
     }
 
-    private void drawAlignText(){
+    private void drawAlignText(Canvas canvas){
+
+        setTextPaint();
+
+        String content = getText().toString();
+        int currentOffsetY = getPaddingTop();
+        currentOffsetY += getTextSize();
+
+        int lines = mLayout.getLineCount();
+        for (int i = 0; i < lines; i ++){
+            int lineStart = mLayout.getLineStart(i);
+            int lineEnd = mLayout.getLineEnd(i);
+            float wordsWidth = StaticLayout.getDesiredWidth(content, lineStart, lineEnd, mTextPaint);
+            String line = content.substring(lineStart, lineEnd);
+            if (judgeLineNeedZoom(line)){
+                if (i == (lines - 1)){
+                    canvas.drawText(line, getPaddingLeft(), currentOffsetY, mTextPaint);
+                }
+                else {
+                    drawAlignLineText(canvas, line, wordsWidth, currentOffsetY);
+                }
+            }
+            else {
+                canvas.drawText(line, getPaddingLeft(), currentOffsetY, mTextPaint);
+            }
+            currentOffsetY += getLineHeight();
+        }
+    }
+
+    /**
+     * 设置TextPaint
+     */
+    private void setTextPaint() {
+        mTextPaint.setColor(getCurrentTextColor());
+        mTextPaint.drawableState = getDrawableState();
+    }
+
+    /**
+     * 判断改行是否需要进行缩放
+     * @param line
+     *          改行内容字符串
+     * @return
+     *          返回判断结果
+     *          true：改行内容需要进行缩放
+     *          false：该行没有内容，或者该行为文本结尾
+     */
+    private boolean judgeLineNeedZoom(String line){
+        if (line.length() == 0){
+            return false;
+        }
+        return line.charAt(line.length() - 1) != '\n';
+    }
+
+    private void drawAlignLineText(Canvas canvas, String line, float Width, int offsetY){
 
     }
 }
